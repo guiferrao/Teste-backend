51fb884d8565dabf3657642214e438ed6fbb4697
guiferrao <ferraogui@hotmail.com>
2026-02-26 10:59:27 -0300

    feat: Configuração inicial e instalação dos pacotes do sqlite

---
+++ src/Teste.Api/Data/TesteDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Teste.Api.Models;
+
+namespace Teste.Api.Data
+{
+    public class TesteDbContext : DbContext
+    {
+        public TesteDbContext(DbContextOptions<TesteDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ src/Teste.Api/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿namespace Teste.Api.Models
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime AtualizadoEm { get; set; }
+    }
+}
